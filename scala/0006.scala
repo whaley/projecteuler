9a4719573af10a9d36c2817dcf84af1b734d07b1
Jason Whaley <jasonwhaley@gmail.com>
2015-10-12 10:50:37 -0400

    Solve problem 6 with brute force

---
+++ scala/0006.scala
@@ -0,0 +1,22 @@
+def sumOfSquares(n: Int): Int = {
+  @annotation.tailrec
+  def loop(n: Int, accum: Int): Int = {
+    if (n < 1) accum
+    else loop(n - 1, accum + (n * n))
+  }
+  loop(n, 0)
+}
+
+def squareOfSums(n: Int) = {
+    @annotation.tailrec
+    def sum(n: Int, accum: Int): Int = {
+      if (n < 1) accum
+      else sum(n - 1, accum + n)
+    }
+    val sumAll = sum(n,0)
+    sumAll * sumAll
+}
+
+def diff(n: Int): Int = {
+  
+}
